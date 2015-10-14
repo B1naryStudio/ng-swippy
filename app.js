e34a4a06e67b13efdef6fec131b0608470a426bf
Bogdan Rusinka <rusinka.bogdan@gmail.com>
2015-10-14 18:09:23 +0300

    Limit shown image count to 4 instead of unlimit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('ngSwippyDemo', ['ngSwippy'])
+angular.module('ngSwippyDemo', ['ngSwippy', 'ngMaterial'])
 	.controller('MainController', function($scope, $timeout){
 		$scope.itemsCollection = [{
 			thumbnail: 'images/1.jpg',
@@ -12,5 +12,37 @@ angular.module('ngSwippyDemo', ['ngSwippy'])
 			thumbnail: 'images/3.jpg',
 			title: 'Rose Tyler',
 			subtitle: 'rose@gmail.com',
+		},{
+			thumbnail: 'images/4.jpg',
+			title: 'Donna Noble',
+			subtitle: 'donna@gmail.com',
+		},{
+			thumbnail: 'images/5.jpg',
+			title: 'Martha Jones',
+			subtitle: 'martha@gmail.com',
 		}];
+
+		$scope.myCustomFunction = function(){
+			$timeout(function(){
+				$scope.clickedTimes = $scope.clickedTimes + 1;
+			});
+			
+		};
+
+		$scope.size = {
+			width: 300,
+			height: 200
+		};
+
+		$scope.showinfo = true;
+
+		$scope.swipeend = false;
+
+		$scope.clickedTimes = 0;
+
+		$scope.$watch('swipeend', function(el){
+			if (el){
+				alert('Done!');
+			}
+		});
 	});
