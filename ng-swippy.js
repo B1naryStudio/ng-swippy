19ab2d2fdea65db39aaa4e2e6a7d99ca19e7d5bb
Bogdan Rusinka <rusinka.bogdan@gmail.com>
2015-10-15 16:59:50 +0300

    Update prod file

--- ng-swippy.js
+++ ng-swippy.js
@@ -1,4 +1,100 @@
-angular.module('ngSwippy', ['ngTouch']).
+angular.module('ngSwippy', ['ngTouch'])
+	.factory('swipe', [function() {
+
+	  var MOVE_BUFFER_RADIUS = 0;
+
+	  var POINTER_EVENTS = {
+	    'mouse': {
+	      start: 'mousedown',
+	      move: 'mousemove',
+	      end: 'mouseup'
+	    },
+	    'touch': {
+	      start: 'touchstart',
+	      move: 'touchmove',
+	      end: 'touchend',
+	      cancel: 'touchcancel'
+	    }
+	  };
+
+	  function getCoordinates(event) {
+	    var originalEvent = event.originalEvent || event;
+	    var touches = originalEvent.touches && originalEvent.touches.length ? originalEvent.touches : [originalEvent];
+	    var e = (originalEvent.changedTouches && originalEvent.changedTouches[0]) || touches[0];
+
+	    return {
+	      x: e.clientX,
+	      y: e.clientY
+	    };
+	  }
+
+	  function getEvents(pointerTypes, eventType) {
+	    var res = [];
+	    angular.forEach(pointerTypes, function(pointerType) {
+	      var eventName = POINTER_EVENTS[pointerType][eventType];
+	      if (eventName) {
+	        res.push(eventName);
+	      }
+	    });
+	    return res.join(' ');
+	  }
+
+	  return {
+	   
+	    bind: function(element, eventHandlers, pointerTypes) {
+
+	      var totalX, totalY;
+
+	      var startCoords;
+
+	      var lastPos;
+
+	      var active = false;
+
+	      pointerTypes = pointerTypes || ['mouse', 'touch'];
+	      element.on(getEvents(pointerTypes, 'start'), function(event) {
+	        startCoords = getCoordinates(event);
+	        active = true;
+	        totalX = 0;
+	        totalY = 0;
+	        lastPos = startCoords;
+	        eventHandlers['start'] && eventHandlers['start'](startCoords, event); // jshint ignore:line
+	      });
+	      var events = getEvents(pointerTypes, 'cancel');
+	      if (events) {
+	        element.on(events, function(event) {
+	          active = false;
+	          eventHandlers['cancel'] && eventHandlers['cancel'](event); // jshint ignore:line
+	        });
+	      }
+
+	      element.on(getEvents(pointerTypes, 'move'), function(event) {
+	        if (!active) return;
+
+	        if (!startCoords) return;
+	        var coords = getCoordinates(event);
+
+	        totalX += Math.abs(coords.x - lastPos.x);
+	        totalY += Math.abs(coords.y - lastPos.y);
+
+	        lastPos = coords;
+
+	        if (totalX < MOVE_BUFFER_RADIUS && totalY < MOVE_BUFFER_RADIUS) {
+	          return;
+	        }
+	          
+	          eventHandlers['move'] && eventHandlers['move'](coords, event); // jshint ignore:line
+
+	      });
+
+	      element.on(getEvents(pointerTypes, 'end'), function(event) {
+	        if (!active) return;
+	        active = false;
+	        eventHandlers['end'] && eventHandlers['end'](getCoordinates(event), event); // jshint ignore:line
+	      });
+	    }
+	  };
+	}]).
 	directive('ngSwippy', ['swipe', function(swipe){
 		return {
 			restrict: 'E',
